6f1e4210a6b1bd6e4075b9b1e3c341609d36cb49
Eddie Jaoude <eddie@jaoudestudios.com>
2024-05-27 08:39:23 +0100

    feat: basic page layout

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
+          <span className="block sm:inline">
+            &copy; 2021 Your Company, Inc.
+          </span>{" "}
+          <span className="block sm:inline">All rights reserved.</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
