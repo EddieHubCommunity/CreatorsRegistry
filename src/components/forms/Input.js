f4a35dd2f1155c9e6006e36e6e26d1d5f4937ba9
Eddie Jaoude <eddie@jaoudestudios.com>
2024-06-03 07:13:06 +0100

    fix: disable input on submit

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -1,7 +1,10 @@
-import { classNames } from "@/utils/classNames";
+import { useFormStatus } from "react-dom";
 import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
 
+import { classNames } from "@/utils/classNames";
+
 export default function Input({ name, value, error }) {
+  const { pending } = useFormStatus();
   return (
     <div className="sm:col-span-4">
       <label
@@ -13,13 +16,15 @@ export default function Input({ name, value, error }) {
       <div className="relative mt-2 rounded-md shadow-sm">
         <input
           type="text"
+          disabled={pending}
           name={name}
           id={name}
           className={classNames(
-            "block w-full rounded-md border-0 py-1.5 pr-10  ring-1 ring-inset  focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6",
+            "block w-full rounded-md border-0 py-1.5 pr-10  ring-1 ring-inset focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6",
             error
               ? "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500"
-              : "ring-gray-300 focus-within:ring-indigo-600"
+              : "ring-gray-300 focus-within:ring-indigo-600",
+            pending && "bg-gray-300"
           )}
           aria-invalid="true"
           aria-describedby={`${name}-error`}
--- src/components/forms/SubmitButton.js
+++ src/components/forms/SubmitButton.js
@@ -10,8 +10,8 @@ export function SubmitButton() {
       type="submit"
       disabled={pending}
       className={classNames(
-        "rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-        pending ? "bg-grey-600" : "bg-indigo-600"
+        "rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+        pending ? "bg-gray-600" : "bg-indigo-600 hover:bg-indigo-500"
       )}
     >
       Save
