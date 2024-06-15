e060ed44066bbbb4faf267f70b2e8367ad72e9db
Eddie Jaoude <eddie@jaoudestudios.com>
2024-06-15 10:46:26 +0100

    feat: select form field

---
+++ src/components/forms/Select.js
@@ -0,0 +1,44 @@
+import { useFormStatus } from "react-dom";
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
+
+import { classNames } from "@/utils/classNames";
+
+export default function Select({ name, options = [], value, error }) {
+  const { pending } = useFormStatus();
+
+  return (
+    <div className="col-span-full">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {name}
+      </label>
+      <div className="mt-2">
+        <select
+          id={name}
+          name={name}
+          defaultValue={value}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+        >
+          {options.map((option) => (
+            <option value={option.value}>{option.display}</option>
+          ))}
+        </select>
+        {error && (
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <ExclamationCircleIcon
+              className="h-5 w-5 text-red-500"
+              aria-hidden="true"
+            />
+          </div>
+        )}
+      </div>
+      {error && (
+        <p className="mt-2 text-sm text-red-600" id={`${name}-error`}>
+          {error}
+        </p>
+      )}
+    </div>
+  );
+}
