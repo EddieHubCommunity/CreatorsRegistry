ae249ca1cd4aaa3c3eac40fc2540a5ce9447e485
Eddie Jaoude <eddie@jaoudestudios.com>
2024-06-16 11:51:03 +0100

    fix: select display default option

--- src/components/forms/Select.js
+++ src/components/forms/Select.js
@@ -22,7 +22,9 @@ export default function Select({ name, options = [], value, error }) {
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
         >
           {options.map((option) => (
-            <option value={option.value}>{option.display}</option>
+            <option value={option.value} selected={option.value === value}>
+              {option.display}
+            </option>
           ))}
         </select>
         {error && (
