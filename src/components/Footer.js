4d75af576535f756ce5995872e7feee2dfda943f
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-25 10:06:40 +0530

    feat: github-link-to-repo-in-footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,29 @@
+import { SocialIcon } from 'react-social-icons';
 export default function Footer() {
-  return (
-    <footer>
-      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
-          <span className="block sm:inline">
-            &copy; {new Date().getFullYear()} EddieHub
-          </span>{" "}
-          <span className="block sm:inline">All rights reserved.</span>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="border-t py-8 border-gray-200 flex flex-row items-center justify-between gap-4">
+                    <div className="text-center text-sm text-gray-500 sm:text-left">
+                        <span className="block sm:inline">
+                            &copy; {new Date().getFullYear()} EddieHub
+                        </span>{' '}
+                        <span className="block sm:inline">
+                            All rights reserved.
+                        </span>
+                    </div>
+                    <div className="text-sm text-gray-500">
+                        <div className="block cursor-pointer">
+                            <SocialIcon
+                                style={{ width: 28, height: 28 }}
+                                network="github"
+                                url="https://github.com/EddieHubCommunity/CreatorsRegistry"
+                                target="_blank"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
