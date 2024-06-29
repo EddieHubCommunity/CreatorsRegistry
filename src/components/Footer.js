0d17f12cc8099a215af00d6de259524cc9116449
dev241202@gmail.com <dev241202@gmail.com>
2024-06-29 08:24:46 +0530

    feat: Added GitHub link in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
 import data from "@/config/app.json";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
-          <span className="block sm:inline">
-            &copy; {new Date().getFullYear()} EddieHub
-          </span>{" "}
-          <span className="block sm:inline">
-            All rights reserved | v{data.version}
-          </span>
+    <footer className="border-t border-gray-200 py-8">
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex justify-between items-center">
+        <div className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} EddieHub All rights reserved | v{data.version}
         </div>
+        <a
+          href="https://github.com/EddieHubCommunity/CreatorsRegistry"
+          className="text-gray-500 hover:text-gray-600"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="h-6 w-6 inline-block mb-1" />
+        </a>
       </div>
     </footer>
   );
