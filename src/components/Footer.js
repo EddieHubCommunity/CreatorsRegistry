ddbe0cbd0982e815be857728de4771083afc9b57
Devesh Rahatekar <79015420+devesh-2002@users.noreply.github.com>
2024-06-30 19:27:09 +0530

    feat: GitHub link in footer (#110)
    * feat: Added GitHub link in Footer
    
    * Removed icon and added URL
    
    * Removed passHref
    
    * package-lock.json related changes
    
    * package-lock.json related changes 2
    
    * feat: Added GitHub Link in Footer
    
    * Update src/components/Footer.js
    
    * Update src/components/Footer.js
    
    * Update src/components/Footer.js
    
    * Update src/components/Footer.js
    
    ---------
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,17 @@
 import data from "@/config/app.json";
+import Link from "next/link";
 
 export default function Footer() {
+  const githubUrl = "https://github.com/EddieHubCommunity/CreatorsRegistry";
+
   return (
-    <footer>
-      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
-          <span className="block sm:inline">
-            &copy; {new Date().getFullYear()} EddieHub
-          </span>{" "}
-          <span className="block sm:inline">
-            All rights reserved | v{data.version}
-          </span>
+    <footer className="border-t border-gray-200 py-8">
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex justify-between items-center">
+        <div className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} EddieHub All rights reserved | v
+          {data.version}
         </div>
+        <Link href={githubUrl}>{githubUrl}</Link>
       </div>
     </footer>
   );
