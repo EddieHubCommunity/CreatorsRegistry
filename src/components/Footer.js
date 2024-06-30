868e03105532584c4695f1d7995fe64987aefb57
Eddie Jaoude <eddie@jaoudestudios.com>
2024-06-30 14:55:49 +0100

    Update src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,8 @@ export default function Footer() {
     <footer className="border-t border-gray-200 py-8">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex justify-between items-center">
         <div className="text-sm text-gray-500">
-          &copy; {new Date().getFullYear()} EddieHub All rights reserved | v{data.version}
+          &copy; {new Date().getFullYear()} EddieHub All rights reserved | v
+          {data.version}
         </div>
         <Link href={githubUrl}>
             {githubUrl}
